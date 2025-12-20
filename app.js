94178e9477d5d970a225865ed02809d3dbe4fdf8
tamannachopraaa <tamanna202chopra@gmail.com>
2025-12-20 09:18:19 +0530

    deploy ready

--- app.js
+++ app.js
@@ -457,3 +457,9 @@ wss.on('connection', ws => {
 
 // --- EXPORT THE APP AND SERVER ---
 module.exports = { app, server };
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
